637757f99b57d483c664e0c22638b2ee20c9500d
phuoc <phuoc1999qn1@gmail.com>
2020-10-20 18:34:34 +0700

    .

--- type-racer/src/app/app.module.ts
+++ type-racer/src/app/app.module.ts
@@ -14,7 +14,7 @@ import { HeaderComponent } from './home/components/header/header.component';
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
--- type-racer/src/app/home/components/header/header.component.ts
+++ type-racer/src/app/home/components/header/header.component.ts
@@ -1,15 +1,62 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { auth } from 'firebase';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  constructor(
+    public afAuth: AngularFireAuth,
+    private authService: AuthService
+  ) {}
 
-  constructor() { }
+  user: any = {
+    name: '',
+    ava: '',
+  };
 
   ngOnInit(): void {
+    this.checkedLogin();
   }
 
+  checkedLogin() {
+    this.authService.isAuth().subscribe((auth) => {
+      if (auth) {
+        this.user.name = auth.displayName;
+        this.user.ava = auth.photoURL;
+        console.log(auth);
+      } else  {
+        console.log('abc');
+        this.user = null;
+      }
+    });
+  }
+
+  onLoginFb() {
+    // this.afAuth.signInWithPopup(new auth.FacebookAuthProvider());
+    return new Promise<any>((resolve, reject) => {
+      let provider = new auth.FacebookAuthProvider();
+      this.afAuth
+      .signInWithPopup(provider)
+      .then(res => {
+        resolve(res);
+        console.log(res);
+      }, err => {
+        console.log(err);
+        reject(err);
+      })
+    })
+  }
+
+  onGoogleFb() {
+    this.afAuth.signInWithPopup(new auth.GoogleAuthProvider());
+  }
+
+  onLogout() {
+    this.afAuth.signOut();
+  }
 }
