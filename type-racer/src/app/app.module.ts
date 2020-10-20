dbbefa78a2a3ec40c233475f5d18d41da8575a58
phuoc <phuoc1999qn1@gmail.com>
2020-10-20 12:09:14 +0700

    first commit

---
+++ type-racer/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import {AngularFireModule} from '@angular/fire';
+k
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ type-racer/src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBi0WhF3gqlQMEL3HKLtgN7htX6BJvTe1Y',
+    authDomain: 'typeracer-5a607.firebaseapp.com',
+    databaseURL: 'https://typeracer-5a607.firebaseio.com',
+    projectId: 'typeracer-5a607',
+    storageBucket: 'typeracer-5a607.appspot.com',
+    messagingSenderId: '552019355137',
+    appId: '1:552019355137:web:2a87a3b2174356f1504200',
+    measurementId: 'G-N95NEQ3STK',
+  },
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
