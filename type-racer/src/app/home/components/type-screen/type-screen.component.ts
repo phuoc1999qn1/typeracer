b79847c35911cf592f1eb49213c239de3dcad082
Bui Tuan <58723584+thanhtuan76@users.noreply.github.com>
2020-10-26 11:39:28 +0700

    Add move function

--- type-racer/src/app/home/components/type-screen/type-screen.component.ts
+++ type-racer/src/app/home/components/type-screen/type-screen.component.ts
@@ -11,35 +11,41 @@ declare var $;
 export class TypeScreenComponent implements OnInit {
   item: any[];
   random: number;
-  constructor(public db: AngularFireDatabase) { }
+  paragraph: string[];
+  index = 0;
+  run = 10;
+
+  constructor(public db: AngularFireDatabase) {
+
+  }
 
   ngOnInit(): void {
     $('#myTab a[href="#profile"]').tab('show'); // Select tab by name
-    this.process();
     this.splitWord();
-    
+    this.process();
   }
 
+  // Kiểm tra nội dung nhập trùng khớp với văn bản đưa ra
   process() {
     let quoteInputElement = document.getElementById('typeInput');
+    //hien thi para
     let quoteDisplay = document.getElementById('quote');
-    let flag = false;
 
     quoteInputElement.addEventListener('input', () => {
       let arrayQuote = quoteDisplay.querySelectorAll('span');
       let arrayValue = $('#typeInput').val().split('');
 
+
+
       arrayQuote.forEach((characterSpan, index) => {
-        let character = arrayValue[index];
+        let characterInput = arrayValue[index];
 
-        if (character === ' ') {
-          setInterval(this.move, 200)
-        }
+        // console.log('pro');
 
-        if (character == null) {
+        if (characterInput == null) {
           characterSpan.style.color = '#000';
           characterSpan.style.backgroundColor = '#ececec';
-        } else if (character === characterSpan.innerText) {
+        } else if (characterInput === characterSpan.innerText) {
           characterSpan.style.color = '#3bbb1b';
           characterSpan.style.backgroundColor = '#ececec';
         } else {
@@ -49,16 +55,49 @@ export class TypeScreenComponent implements OnInit {
     });
   }
 
-  move() {
-    let quoteDisplay = document.getElementById('quote');
-    let move = document.getElementById('run')
-    let max = quoteDisplay.innerText.length
+  move(a) {
+    a.value = null;
+    a.placeholder = '';
+    // let quoteDisplay = document.getElementById('quote');
+    let inputValue = $('#typeInput').val();
+    // let temp = document.getElementById('typeInput');
+    // this.paragraph = quoteDisplay.innerText.split(' ');
 
-    for (let i = 0; i <= max; i++) {
-      move.style.paddingLeft = i + "px";
+
+    // temp.nodeValue = null;
+    // console.log(this.paragraph[1])
+    // console.log(this.paragraph.length)
+
+    // console.log(quoteDisplay.innerText.split(' '))
+
+    // for (let i = 0; i < this.paragraph.length; i++) {
+
+    // }
+    // console.log(a.split(' ')[index])
+
+    // console.log(input.value.trim())
+    if (this.index < this.paragraph.length) {
+      if (inputValue.split(' ')[this.index] === this.paragraph[this.index]) {
+        // console.log(inputValue.split(' ')[this.index])
+        // this.paragraph = this.paragraph.filter(() => this.paragraph.shift());
+        // console.log(this.paragraph)
+        this.paragraph.shift();
+        console.log(this.paragraph)
+        this.index++;
+        this.run += (90 / this.paragraph.length);
+        $('.progress-bar').css("width", this.run + "%");
+        $('.car-info').css("padding-left", this.run - 10 + "%")
+      }
+      else {
+        console.log("Error!!!")
+      }
     }
+
+
   }
 
+
+  // Tách nội dung văn bản thành từng kí tự
   splitWord() {
     this.db
       .list('paragraph')
@@ -75,8 +114,6 @@ export class TypeScreenComponent implements OnInit {
         });
       });
   }
-
-
 
   getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
