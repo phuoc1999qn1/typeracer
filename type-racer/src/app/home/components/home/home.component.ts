deb6ca1f68f71c4d3dbdb1fed5516cfe9c0f45b3
phuoc <phuoc1999qn1@gmail.com>
2020-10-26 11:39:39 +0700

    .

--- type-racer/src/app/home/components/home/home.component.ts
+++ type-racer/src/app/home/components/home/home.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { CreGameService } from 'src/app/services/creGame.service'
 
 @Component({
   selector: 'app-home',
@@ -8,16 +7,9 @@ import { CreGameService } from 'src/app/services/creGame.service'
 })
 export class HomeComponent implements OnInit {
   roomId: string;
-  private creGameService: CreGameService
   constructor() { }
 
   ngOnInit(): void {
-    console.log(this.creGameService.createId()
-    .then( x => {
-      console.log(x)
-    })
-    );
-
   }
 
   createGame() {
